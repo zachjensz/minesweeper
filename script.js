29b5c960b8628e16f020bf886864f855f46f0c9d
Zach <zachjensz@protonmail.com>
2021-05-22 11:05:23 +1000

    Copy pasted Kyle's code

--- script.js
+++ script.js
@@ -1,192 +1,68 @@
-const width = 16
-const height = 9
-const grid = generateLevel(width, height, 0.2)
+import {
+  TILE_STATUSES,
+  createBoard,
+  markTile,
+  revealTile,
+  checkWin,
+  checkLose
+} from './logic.js'
 
-function generateLevel(width, height, minefactor) {
-  // Initialize Variables
-  const grid = []
-  const game = document.querySelector('#game')
+const BOARD_SIZE = 12
+const NUMBER_OF_MINES = 20
 
-  // Style Grid
-  game.style.aspectRatio = `${width} / ${height}`
-  game.style.gridTemplateColumns = `repeat(${width}, 1fr)`
-  game.style.gridTemplateRows = `repeat(${height}, 1fr)`
+const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES)
+const boardElement = document.querySelector('.board')
+const minesLeftText = document.querySelector('[data-mine-count]')
+const messageText = document.querySelector('.subtext')
 
-  // Distribute Mines
-  const mines = width * height * minefactor
-  for (let placed = 0; placed <= mines; placed++) {
-    grid[chooseEmptyTile(grid, width * height)] = 9
-  }
+board.forEach((row) => {
+  row.forEach((tile) => {
+    boardElement.append(tile.element)
+    tile.element.addEventListener('click', () => {
+      revealTile(board, tile)
+      checkGameEnd()
+    })
+    tile.element.addEventListener('contextmenu', (e) => {
+      e.preventDefault()
+      markTile(tile)
+      listMinesLeft()
+    })
+  })
+})
+boardElement.style.setProperty('--size', BOARD_SIZE)
+minesLeftText.textContent = NUMBER_OF_MINES
 
-  // Calculate Numbers
-  for (let v = 0; v < height; v++) {
-    // Rows
-    for (let h = 0; h < width; h++) {
-      // Tiles
-      let tile = v * width + h
-      if (grid[tile] === 9) continue // I'm a mine
-      // I'm not a mine, check surrouding 8 tiles for mines
-      let mines = 0
-      let leftValid = h > 0
-      let rightValid = h + 1 < width
-      if (leftValid && grid[tile - width - 1] === 9) mines++ // Top Left
-      if (grid[tile - width] === 9) mines++ // Top
-      if (rightValid && grid[tile - width + 1] === 9) mines++ // Top Right
-      if (rightValid && grid[tile + 1] === 9) mines++ // Right
-      if (rightValid && grid[tile + width + 1] === 9) mines++ // Bottom Right
-      if (grid[tile + width] === 9) mines++ // Bottom
-      if (leftValid && grid[tile + width - 1] === 9) mines++ // Bottom Left
-      if (leftValid && grid[tile - 1] === 9) mines++ // Left
-      // Set in Array
-      grid[tile] = mines
-    }
-  }
-
-  // Render HTML
-  grid.forEach(renderTile)
-
-  // Return array of tiles
-  return grid
-}
-
-function renderTile(item, index) {
-  const template = document.querySelector('#tile-template')
-  const tile = template.content.cloneNode(true)
-  tile.querySelector('.value').innerText = item
-  if (item === 9) tile.querySelector('.value').innerText = '#'
-  tile.querySelector('.value').setAttribute('data-value', item)
-  tile.querySelector('.value').setAttribute('data-index', index)
-  game.appendChild(tile)
-}
-
-function chooseEmptyTile(grid, tiles) {
-  const tile = Math.floor(Math.random() * tiles)
-  if (grid[tile] === 9) return chooseEmptyTile(grid, tiles)
-  return tile
-}
-// Reveal tiles when clicked
-document.querySelector('#game').onclick = function (e) {
-  if (e.target.classList.contains('cover'))
-    reveal(
-      grid,
-      width,
-      height,
-      e.target.parentNode.querySelector('.value').getAttribute('data-index')
+function listMinesLeft() {
+  const markedTilesCount = board.reduce((count, row) => {
+    return (
+      count + row.filter((tile) => tile.status === TILE_STATUSES.MARKED).length
     )
+  }, 0)
+
+  minesLeftText.textContent = NUMBER_OF_MINES - markedTilesCount
 }
 
-// Create flag on tile when right clicked
-document.querySelector('#game').oncontextmenu = function (e) {
-  e.preventDefault()
-  if (e.target.classList.contains('cover')) {
-    e.target.innerText == '!'
-      ? (e.target.innerText = '')
-      : (e.target.innerText = '!')
+function checkGameEnd() {
+  const win = checkWin(board)
+  const lose = checkLose(board)
+
+  if (win || lose) {
+    boardElement.addEventListener('click', stopProp, { capture: true })
+    boardElement.addEventListener('contextmenu', stopProp, { capture: true })
+  }
+
+  if (win) messageText.textContent = 'You Win'
+  if (lose) {
+    messageText.textContent = 'You Lose'
+    board.forEach((row) => {
+      row.forEach((tile) => {
+        if (tile.status === TILE_STATUSES.MARKED) markTile(tile)
+        if (tile.mine) revealTile(board, tile)
+      })
+    })
   }
 }
 
-// Reveal clicked tiles function
-function reveal(grid, width, height, index) {
-  if (index >= width * height) return
-  const tile = document.querySelector(`[data-index="${index}"]`).parentNode
-
-  // If I'm already revealed, return
-  if (tile.classList.contains('revealed'))
-    if (tile.classList.contains('revealed')) return
-
-  // Reveal me
-  tile.classList.add('revealed')
-
-  // What number do I hold
-  const value = grid[index]
-
-  // If I'm a mine, game over!
-  if (value === 9) gameOver()
-
-  // I'm empty, reveal surrounding 8 tiles
-  if (value === 0) revealSurroundings(grid, width, height, index)
-
-  // Check for surrounding empty tiles
-  recursiveReveal(grid, width, height, index)
-}
-
-// Recursive tiles function
-function recursiveReveal(grid, width, height, index) {
-  // Are there any empty tiles around me?
-  const leftvalid = index % width > 0
-  const rightvalid = (index % width) + 1 < width
-  const abovevalid = index >= width
-  const belowvalid = index <= width * (height - 1)
-  if (leftvalid) {
-    // Left Valid
-    if (abovevalid && grid[index - width - 1] === 0)
-      // Top Left
-      reveal(grid, width, height, index - width - 1)
-    if (belowvalid && grid[index + width - 1] === 0)
-      // Bottom Left
-      reveal(grid, width, height, index + width - 1)
-    if (grid[index - 1] === 0)
-      // Left
-      reveal(grid, width, height, index - 1)
-  }
-  if (rightvalid) {
-    // Right Valid
-    if (abovevalid && grid[index - width + 1] === 0)
-      // Top Right
-      reveal(grid, width, height, index - width + 1)
-    if (grid[index + 1] === 0)
-      // Right
-      reveal(grid, width, height, index + 1)
-    if (belowvalid && grid[index + width + 1] === 0)
-      // Bottom Right
-      reveal(grid, width, height, index + width + 1)
-  }
-  if (abovevalid && grid[index - width] === 0)
-    // Top
-    reveal(grid, width, height, index - width)
-  if (belowvalid && grid[index + width] === 0)
-    // Bottom
-    reveal(grid, width, height, index + width)
-}
-
-// Reveal surrounding tiles function
-function revealSurroundings(grid, width, height, index) {
-  // Reveal surrounding tiles
-  const leftvalid = index % width > 0
-  const rightvalid = (index % width) + 1 < width
-  const abovevalid = index >= width
-  const belowvalid = index <= width * (height - 1)
-  if (leftvalid) {
-    // Left Valid
-    if (abovevalid)
-      // Top Left
-      reveal(grid, width, height, index - width - 1)
-    if (belowvalid)
-      // Bottom Left
-      reveal(grid, width, height, index + width - 1)
-    // Left
-    reveal(grid, width, height, index - 1)
-  }
-  if (rightvalid) {
-    // Right Valid
-    if (abovevalid)
-      // Top Right
-      reveal(grid, width, height, index - width + 1)
-    // Right
-    reveal(grid, width, height, index + 1)
-    if (belowvalid)
-      // Bottom Right
-      reveal(grid, width, height, index + width + 1)
-  }
-  if (abovevalid)
-    // Top
-    reveal(grid, width, height, index - width)
-  if (belowvalid)
-    // Bottom
-    reveal(grid, width, height, index + width)
-}
-
-function gameOver() {
-  alert('Game Over!')
-  location.reload()
+function stopProp(e) {
+  e.stopImmediatePropagation()
 }
